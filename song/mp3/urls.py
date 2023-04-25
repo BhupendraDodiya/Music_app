4eb00bf07f47befa2b3c6d869e68608fafff7b86
Bhupendra Dodiya <bdbhupendra123@gmail.com>
2023-04-25 12:03:31 +0530

    create add_song view from user

--- song/mp3/urls.py
+++ song/mp3/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('',views.index,name='home'),
+    path('add_song/',views.add_song,name='add'),
     path('artist/<str:name>/',views.artist,name='artist'),
     path('song/<int:uid>/',views.song.as_view(),name='song-data'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- song/mp3/views.py
+++ song/mp3/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from .models import Song
 # Create your views here.
@@ -17,5 +17,16 @@ def artist(request,name):
     print(singer)
     return render(request,'artist.html',{'singer':singer})
 
+
+def add_song(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        singer = request.POST['singer']
+        pic = request.FILES['pic']
+        audio = request.FILES['audio']
+        Song.objects.create(song_name=name,singer_name=singer,song_image=pic,song_audio=audio)
+        return redirect('/')
+    else:
+        return render(request,'upload.html')
 
     
